db4f46c8c80d36bfe80cf97625ed95161c109f5c
rubal-98 <bssethi2001@gmail.com>
2017-07-31 18:29:34 +0530

    Create Day 1.swift

---
+++ Day 1.swift
@@ -0,0 +1,17 @@
+var i = 4
+var d = 4.0
+var s = "HackerRank "
+
+// Declare second integer, double, and String variables.
+// Read and save an integer, double, and String to your variables.
+var int = Int(readLine()!)
+var double = Double(readLine()!)
+var st:String=readLine()!
+
+// Print the sum of both integer variables on a new line.
+print(i+int!)
+// Print the sum of the double variables on a new line.
+print(d+double!)
+// Concatenate and print the String variables on a new line
+print(s+st)
+// The 's' variable above should be printed first.
