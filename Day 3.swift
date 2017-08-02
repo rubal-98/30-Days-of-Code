bd1cc1e60aea89282bb018b8533c4b9e33846c21
rubal-98 <bssethi2001@gmail.com>
2017-08-02 11:42:02 +0530

    Create Day 3.swift

---
+++ Day 3.swift
@@ -0,0 +1,11 @@
+import Foundation
+// Enter your code here 
+var n=Int(readLine()!)
+if (n!%2) != 0
+   { print("Weird")}
+else if n!>=2&&n!<=5
+    {print("Not Weird ")}
+else if n!>=6&&n!<=20
+    {print("Weird")}
+else
+    {print("Not Weird ")}
