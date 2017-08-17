6357f10acfc7dce93c03958b58e56448e3083796
SAVREEN <bssethi2001@gmail.com>
2017-08-17 12:33:36 +0530

    Create Day E.swift

--- Day E.swift
+++ Day E.swift
@@ -1,1 +1,27 @@
+import Foundation
 
+class Difference {
+    private var elements = [Int]()
+    var maximumDifference : Int!
+    // Write your code here
+      init(a: [Int]){
+        self.elements=a
+         
+    }
+    func computeDifference(){
+        maximumDifference = 0
+        elements.sort{$0 < $1}
+        maximumDifference = elements.last! - elements.first!
+    } 
+  } // End of Difference class
+
+let n = Int(readLine()!)!
+let a = readLine()!.components(separatedBy: " ").map{ Int($0)! }
+
+let d = Difference(a: a)
+
+d.computeDifference()
+
+print(d.maximumDifference)
+
+  
