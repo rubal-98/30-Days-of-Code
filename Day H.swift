f8d2a02376ad81417b4ad7364c3dbe1f866048c9
SAVREEN <bssethi2001@gmail.com>
2017-08-19 18:56:18 +0530

    Create Day H.swift

---
+++ Day H.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+// Defining enum for throwing error
+// throw RangeError.NotInRange... is used to throw the error
+enum RangeError : Error {
+    case NotInRange(String)
+}
+// Start of class Calculator
+class Calculator {
+    // Start of function power
+    func power(n: Int, p: Int) throws -> Int {
+        // Add your code here
+        if n < 0 || p < 0
+         {
+            throw RangeError.NotInRange("n and p should be non-negative")
+         } 
+         else
+         {             
+            return Int(pow(Double(n),Double(p)))
+         }
+          
+    } // End of function power
+} // End of class Calculator
+let myCalculator = Calculator()
+let t = Int(readLine()!)!
+
+for _ in 0..<t {
+    let np = readLine()!.components(separatedBy: " ").map{ Int($0)! }
+    let n = np[0]
+    let p = np[1]
+
+    do {
+        let ans = try myCalculator.power(n: n, p: p)
+        print(ans)
+    } catch RangeError.NotInRange(let errorMsg) {
+        print(errorMsg)
+    }
+}
